2b0c53b8561546f0ddc85d9afebe1a16cf6f672d
Abednego Wambua <abednego.k.wambua@gmail.com>
2019-03-28 18:50:00 +0300

    first commit

---
+++ src/main/java/com/course/demo/HomeController.java
@@ -0,0 +1,14 @@
+package com.course.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+
+        return "hi";
+    }
+}
---
+++ src/main/java/com/course/demo/topics/TopicsService.java
@@ -0,0 +1,48 @@
+package com.course.demo.topics;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicsService {
+
+    private List<Topic> topicList = new ArrayList<>(Arrays.asList(
+
+            new Topic("_spring", "_Spring FrameWork", "_Spring Description"),
+            new Topic("_node_js", "Node Js FrameWork", "Node Description"),
+            new Topic("angular", "Java FrameWork", "Java Description")
+
+    ));
+
+    public List<Topic> getAllTopicList() {
+        return topicList;
+    }
+
+    public Topic getTopic(String id) {
+        return topicList.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
+    }
+
+
+    public List<Topic> addTopic(Topic topic) {
+        topicList.add(topic);
+        return topicList;
+    }
+
+
+    public void updateTopic(Topic topic, String id) {
+        int counter = 0;
+        for (Topic topic1 : topicList) {
+            if (topic1.getId().equals(id)) {
+                topicList.set(counter, topic);
+            }
+            counter++;
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topicList.removeIf(topic -> topic.getId().equals(id));
+    }
+}
